f4dfe842e19b293d51325b5b4f31f1354953d5f2
Karl Stenerud <kstenerud@gmail.com>
2017-11-20 19:12:32 +0100

    Working hive

--- hive/src/tmp/dbinit.sql
+++ hive/src/tmp/dbinit.sql
@@ -1,2 +1,8 @@
-create role testuser superuser login password 'testuser';
-create database test with owner testuser;
+-- psql -U testuser -d test -f this_file.sql
+create schema test;
+use test;
+create table a (a0 int, a1 int, a2 int, a3 int, a4 int, a5 int, a6 int, a7 int);
+create table b (b0 int, b1 int, b2 int, b3 int, b4 int, b5 int, b6 int, b7 int);
+create table c (c0 int, c1 int, c2 int, c3 int);
+create table d (d0 int, d1 int, d2 int, d3 int);
+create table p (p0 int, p1 int, p2 varchar(200), p3 varchar(200), p4 boolean, p5 boolean);
